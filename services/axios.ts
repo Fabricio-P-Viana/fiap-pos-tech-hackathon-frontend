0f97d82e07c330cb506c4efff204a2fbc4d2a458
Fabricio <fabriciopviana@outlook.com>
2026-09-15 11:16:41 -0300

    fix: remove trailing slashes from API base URLs

--- services/axios.ts
+++ services/axios.ts
@@ -4,7 +4,7 @@ import { env } from "process";
 const backendApi = axios.create({
   baseURL:
     env.NEXT_PUBLIC_API_BASE_URL ??
-    "https://hackathon-backend-sx6h.onrender.com/",
+    "https://hackathon-backend-sx6h.onrender.com",
   timeout: 10000,
 });
 
